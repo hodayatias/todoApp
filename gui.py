3feb7525372f5d41bd62d416478b5bdff8de6353
hodaya <hodayatias1@gmail.com>
2025-08-06 12:57:46 +0300

    finished gui

--- gui.py
+++ gui.py
@@ -1,16 +1,25 @@
 import functions
 import FreeSimpleGUI as sg
 import time
+import os
+
+if not os.path.exists('todo.txt'):
+    with open('todo.txt', 'w') as f:
+        pass
 
 sg.theme('Light Purple')
+
 clock = sg.Text('',key='clock')
 label = sg.Text("Type in a to-do")
+
 input_box = sg.InputText(tooltip='Enter a to-do', key='todo')
 add_button = sg.Button('Add', size=(10,1))
+
 listbox = sg.Listbox(values=functions.get_todos(),key='todos',
                      enable_events=True, size=(45,10))
 edit_button = sg.Button('Edit')
 complete_button = sg.Button('Complete')
+
 exit_button = sg.Button('Exit')
 
 window=sg.Window('My To-Do App',
