9c8fc6ea75a66a798801224a414e968dc0aea6ac
hodaya <hodayatias1@gmail.com>
2025-08-06 12:57:07 +0300

    implements 'add' feature

--- web.py
+++ web.py
@@ -3,12 +3,19 @@ import functions
 
 todos=functions.get_todos()
 
+def add_todo():
+    todo=st.session_state["new_todo"]+'\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+    st.session_state["new_todo"]=""
+
+
 st.title('My To-Do App')
 st.write("This app is to increase your productivity")
 
 for item in todos:
     st.checkbox(item)
 
-st.text_input(label='',placeholder='Enter new todo...')
+st.text_input(label='',placeholder='Enter new todo...',on_change=add_todo,key='new_todo')
 
 
